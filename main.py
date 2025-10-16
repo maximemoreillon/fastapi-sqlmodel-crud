93353f778be49f1aeba2fe85a2b59967083c644b
Maxime Moreillon <m.moreillon@gmail.com>
2025-10-16 15:31:07 +0900

    @asynccontextmanager

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from routes import items
 from db import create_tables
+from contextlib import asynccontextmanager
 
 app = FastAPI()
 
 app.include_router(items.router)
 
 
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_tables()
+    yield
